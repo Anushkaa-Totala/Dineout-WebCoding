eee5849bf528b9bfea94a2af49eb54ac6eb47f90
Anushkaa-Totala <anushkatotala01@gmail.com>
2021-09-19 21:17:45 +0530

    Update Login.js

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import LoginGrid from '../Components/LoginPage/LoginGrid';
-import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
 
 export default function Home() {
   return (
     <React.Fragment>
       <Container>
-        <AccessAlarmIcon/>
-<LoginGrid/>
-<ExitToAppIcon/>
+
+        <LoginGrid />
 
       </Container>
 
